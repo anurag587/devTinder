da0cbea9c39e6e72b58c0ea7d720c034983c5387
Anurag Sharma <anuraguict09@gmail.com>
2024-10-04 15:37:14 +0530

    exploring get,post,del http methods

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@ const express = require("express");
 
 const app = express();
 
-//this is known as request handleres
- app.use("/",(req,res)=>{
-    res.send("hello from the server");
+//this is known as request handlers and the order of req handlers are very important 
+app.get("/user",(req,res)=>{
+    res.send({
+        firstName : "Anurag",
+        lastName : "Sharma"
+    });
  });
+
+ app.post("/user",(req,res)=>{
+    res.send("Saving the Data to DB");
+ });
+
+ app.delete("/user",(req,res)=>{
+    res.send("Deleting the Data from DB");
+ });
+
+app.use("/test",(req,res)=>{
+   res.send("testing the api calls");
+});
+
 
 app.listen(3000,()=>{
     console.log("Server Started")
