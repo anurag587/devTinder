04a68cc02193989a4c0c3050ca3bd50c10c0e524
Anurag Sharma <anuraguict09@gmail.com>
2024-10-05 17:05:42 +0530

    dynamic saving user data in DB

--- src/app.js
+++ src/app.js
@@ -4,24 +4,19 @@ const app = express();
 
 const User = require("./models/user");
 
-app.post("/signup", async (req,res)=>{
-    const user = new User({
-        firstName : "Anurag",
-        lastName : "Sharma",
-        emailId : "anuragusict09@gmail.com",
-        password : "anurag@1230",
-        age: 24,
-        gender : "male"
-    })
-    try{
-        await user.save();
-    res.send("User Data Saved")
-    }
-    catch(err){
-        res.status(400).send("Data not Saved")
-    }
-})
 
+app.use(express.json());
+
+
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("User Data Saved");
+  } catch (err) {
+    res.status(400).send("Data not Saved");
+  }
+});
 
 connectDb()
   .then(() => {
