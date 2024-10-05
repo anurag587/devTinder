397df821e0d6e222b9bd7b41b1099ef22b5e1b93
Anurag Sharma <anuraguict09@gmail.com>
2024-10-05 17:45:34 +0530

    GET API

--- src/app.js
+++ src/app.js
@@ -7,6 +7,28 @@ const User = require("./models/user");
 
 app.use(express.json());
 
+//Get User from email
+app.get("/user",async (req,res)=>{
+    const user = await User.findOne({emailId : req.body.emailId});
+    try{
+        res.send(user);
+    }
+    catch(err){
+        res.status(400).send("Something went Wrong")
+    }
+})
+
+//Feed API --> To get all users from DB 
+app.get("/feed", async (req,res)=>{
+    const users = await User.find({});
+    try{
+        res.send(users)
+    }
+    catch(err){
+        res.status(400).send("Something went Wrong")
+    }
+})
+
 
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
