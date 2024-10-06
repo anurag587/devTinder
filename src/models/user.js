e44cdea925d2fdcf845b4fb0a1619639c60e19af
Anurag Sharma <anuraguict09@gmail.com>
2024-10-06 15:08:08 +0530

    Used Validator to validate the User Schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator")
 
 const userSchema = new mongoose.Schema(
   {
@@ -18,10 +19,20 @@ const userSchema = new mongoose.Schema(
       unique: true,
       lowercase: true,
       trim: true,
+      validate(value){
+        if(!validator.isEmail(value)){
+          throw new Error("Invalid EmailId "+ value )
+        }
+      }
     },
     password: {
       type: String,
       minLength: 6,
+      validate(value){
+        if(!validator.isStrongPassword(value)){
+          throw new Error("Not a Strong Password "+ value )
+        }
+      }
     },
     age: {
       type: Number,
@@ -39,8 +50,13 @@ const userSchema = new mongoose.Schema(
     },
     photoUrl: {
       type: String,
+      // validate(value){
+      //   if(!validator.isDataURI(value)){
+      //     throw new Error("Invalid URL "+ value )
+      //   }
+      // },
       default:
-        "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp",
+        "https://depositphotos.com/vector/vector-user-icon-35717211.html",
     },
     skills: {
       type: [String],
